d2c3528dd5da64868632e3447621474989c99ec5
Bruce <bn22@uw.edu>
2015-09-21 11:10:28 -0700

    Refactored Javascript sponsors but didn't add the new nav bar since it was buggier than I thought

--- js/app.js
+++ js/app.js
@@ -6,7 +6,8 @@ $(document).ready(function() {
     $('#name').css("height", $('nav').height() / 2);
     $(document).on('resize', function() {
         resizeFontSize();
-    })
+    });
+    populateSponsorsList();
 });
 
 $(window).resize(function() {
@@ -35,48 +36,8 @@ function resizeFontSize() {
     $('html').css('font-size', window.rem);
 }
 
-var sponsorWebsite = ["http://www.foster.washington.edu/Pages/home.aspx", "http://www.me.washington.edu/", "http://www.engr.washington.edu/", "http://ghaasfoundation.org/",
-    "http://www.isograph.com/", "http://www.rinehartmotion.com/", "http://www.amracinginc.com/", "http://www.thegearworks.com/",
-    "http://www.continentaltire.com/www/tires_us_en/", "http://www.boeing.com/", "http://tovcapitalusa.com", "http://www.kvaser.com",
-    "http://www.dynojet.com/", "http://www.eere.energy.gov/vehiclesandfuels", "http://www.gm.com/", "http://www.nrcan.gc.ca/", "http://www.mathworks.com/",
-    "http://www.arb.ca.gov/", "http://www1.eere.energy.gov/cleancities/", "http://www.dspaceinc.com/", "http://www.a123systems.com/",
-    "http://www.freescale.com/", "http://www.avl.com/", "http://www.nsf.gov/", "http://www.etas.com/", "http://www.snapon.com/",
-    "http://www.trcpg.com", "http://www.magnapowertrain.com/", "http://www.bosch.com/", "http://www.crosschasm.com/", "http://www.siemens.com/plm/",
-    "http://www.cd-adapco.com/", "http://www.vector.com/", "http://www.gkn.com/driveline/Pages/default.aspx", "http://www.sensors-inc.com/",
-    "http://www.neweagle.net/", "http://www.blackberry.com/academic", "http://www.qnx.com", "http://www.woodward.com/",
-    "http://www.gageproducts.com", "http://www.delphi.com", "http://www.cat.com/home", "http://www.ricardo.com",
-    "http://www.proterra.com","http://www.epa.gov/"];
-
-var sponsorLinks = ["img/foster.png","img/ME_UW_purp.png", "img/COE_UW_CTR_purp_h100.jpg", "img/genehaas.png", "img/isograph2.gif", "img/rms.png",
-    "img/amRacing.png", "img/the-gear-works-logo.jpg", "img/continental.png", "img/boeing.png", "img/tov.png", "img/kvaser.png",
-    "img/dynojet.png", "img/us-energy.jpg", "img/gm.jpg", "img/canada.png", "img/math-works.jpg", "img/calepa.png",
-    "img/clean-cities.jpg", "img/dspace.jpg", "img/a123.png", "img/freescale.jpg", "img/avl.jpg", "img/nsf.jpg",
-    "img/etas.jpg", "img/snap-on.jpg", "img/trc.jpg", "img/magna.png", "img/bosch.jpg", "img/logo-crosschasm.png",
-    "img/siemens.jpg", "img/cdadapco.png", "img/vector.png", "img/gkn.jpg", "img/Sensorsogo.png", "img/New Eagle_2013_website.jpg",
-    "img/blackberry.png", "img/qnx.png", "img/woodward.png", "img/Gage_website.jpg", "img/delphi.png", "img/caterpillar.png",
-    "img/ricardo.jpg", "img/proterra.jpg", "img/epa_logo-275x300.jpg"];
-
-var sponsorLevels = ["Platinum Team", "Purple Team", "Green Team", "Headline Competition",
-    "Diamond Competition", "Platinum Competition", "Gold Competition", "Silver Competition", "Bronze Competition"];
-
-var sponsorNumber = [5, 4, 4, 2, 4, 5, 3, 4, 14];
-var currentPosition = 0;
-var sponsor = $('#sponsor');
-sponsorBoard();
-
-function sponsorBoard() {
-    for (var i = 0; i < sponsorLevels.length; i++) {
-        sponsor.append('<hr>');
-        var level = sponsorLevels[i].split(" ");
-        sponsor.append('<p class="level">' + level[0] + '</p>');
-        if (level[1] == "Team") {
-            sponsor.append('<p class="team">' + level[1] + ' Sponsor</p>');
-        } else if (level[1] == "Competition") {
-            sponsor.append('<p class="competition">' + level[1] + ' Sponsor</p>');
-        }
-        for (var j = 0; j < sponsorNumber[i]; j++) {
-            sponsor.append('<a href="' + sponsorWebsite[currentPosition] + '"><img src="' + sponsorLinks[currentPosition] + '"></a>');
-            currentPosition = currentPosition + 1;
-        }
+function populateSponsorsList() {
+    for (var i = 0; i < sponsorsList.length; i++) {
+        $('#Platinum_Team').append('<a href="' + sponsorsList[i].website + '"><img src="' + sponsorsList[i].logo + '"></a>');
     }
 }
---
+++ js/sponsorList.js
@@ -0,0 +1,275 @@
+/**
+ * Created by bruce on 9/21/2015.
+ */
+var sponsorsList = [
+    {
+        name: "Foster School of Business",
+        level:"Platinum_Team",
+        website: "http://www.foster.washington.edu/Pages/home.aspx",
+        logo: "img/foster.png"
+    },
+    {
+        name: "University of Washington Mechanical Engineer",
+        level:"Platinum_Team",
+        website: "http://www.me.washington.edu/",
+        logo: "img/ME_UW_purp.png"
+    },
+    {
+        name: "University of Washington College of Engineering",
+        level:"Platinum_Team",
+        website: "http://www.engr.washington.edu/",
+        logo: "img/COE_UW_CTR_purp_h100.jpg"
+    },
+    {
+        name: "Gene Haas Foundation",
+        level:"Platinum_Team",
+        website: "http://ghaasfoundation.org/",
+        logo: "img/genehaas.png"
+    },
+    {
+        name: "Isograph",
+        level:"Platinum_Team",
+        website: "http://www.isograph.com/",
+        logo: "img/isograph2.gif"
+    },
+    {
+        name: "Rinehart Motion",
+        level:"Purple_Team",
+        website: "http://www.rinehartmotion.com/",
+        logo: "img/rms.png"
+    },
+    {
+        name: "AM Racing",
+        level:"Purple_Team",
+        website: "http://www.amracinginc.com/",
+        logo: "img/amRacing.png"
+    },
+    {
+        name: "The Gear Works",
+        level:"Purple_Team",
+        website: "http://www.thegearworks.com/",
+        logo: "img/the-gear-works-logo.jpg"
+    },
+    {
+        name: "Continental",
+        level:"Purple_Team",
+        website: "http://www.continentaltire.com/www/tires_us_en/",
+        logo: "img/continental.png"
+    },
+    {
+        name: "Boeing",
+        level:"Green_Team",
+        website: "http://www.boeing.com/",
+        logo: "img/boeing.png"
+    },
+    {
+        name: "TOV Capital Markets",
+        level:"Green_Team",
+        website: "http://tovcapitalusa.com",
+        logo: "img/tov.png"
+    },
+    {
+        name: "Kvaser",
+        level:"Green_Team",
+        website: "http://www.kvaser.com",
+        logo: "img/kvaser.png"
+    },
+    {
+        name: "Dynojet",
+        level:"Green_Team",
+        website: "http://www.dynojet.com/",
+        logo: "img/dynojet.png"
+    },
+    {
+        name: "US Department of Energy",
+        level:"Headline_Competition",
+        website: "http://www.eere.energy.gov/vehiclesandfuels",
+        logo: "img/us-energy.jpg"
+    },
+    {
+        name: "General Motors",
+        level:"Headline_Competition",
+        website: "http://www.gm.com/",
+        logo: "img/gm.jpg"
+    },
+    {
+        name: "Natural Resources Canada",
+        level:"Diamond_Competition",
+        website: "http://www.nrcan.gc.ca/",
+        logo: "img/canada.png"
+    },
+    {
+        name: "Math Works",
+        level:"Diamond_Competition",
+        website: "http://www.mathworks.com",
+        logo: "img/math-works.jpg"
+    },
+    {
+        name: "Clean Cities",
+        level:"Diamond_Competition",
+        website: "http://www1.eere.energy.gov/cleancities/",
+        logo: "img/clean-cities.jpg"
+    },
+    {
+        name: "Air Resource Board",
+        level:"Diamond_Competition",
+        website: "http://www.arb.ca.gov/",
+        logo: "img/calepa.png"
+    },
+    {
+        name: "dSpace",
+        level:"Platinum_Competition",
+        website: "http://www.dspaceinc.com/",
+        logo: "img/dspace.jpg"
+    },
+    {
+        name: "A123 Systems",
+        level:"Platinum_Competition",
+        website: "http://www.a123systems.com/",
+        logo: "img/a123.png"
+    },
+    {
+        name: "Freescale",
+        level:"Platinum_Competition",
+        website: "http://www.freescale.com/",
+        logo: "img/freescale.jpg"
+    },
+    {
+        name: "AVL",
+        level:"Platinum_Competition",
+        website: "http://www.avl.com/",
+        logo: "img/avl.jpg"
+    },
+    {
+        name: "National Science Foundation",
+        level:"Platinum_Competition",
+        website: "http://www.nsf.gov/",
+        logo: "img/nsf.jpg"
+    },
+    {
+        name: "ETAs",
+        level:"Gold_Competition",
+        website: "http://www.etas.com/",
+        logo: "img/etas.jpg"
+    },
+    {
+        name: "Span-on",
+        level:"Gold_Competition",
+        website: "http://www.snapon.com/",
+        logo: "img/snap-on.jpg"
+    },
+    {
+        name: "Transportation Research Center",
+        level:"Gold_Competition",
+        website: "http://www.trcpg.com",
+        logo: "img/trc.jpg"
+    },
+    {
+        name: "Magna Powertrain",
+        level:"Silver_Competition",
+        website: "http://www.magnapowertrain.com/",
+        logo: "img/magna.png"
+    },
+    {
+        name: "Bosch",
+        level:"Silver_Competition",
+        website: "http://www.bosch.com/",
+        logo: "img/bosch.jpg"
+    },
+    {
+        name: "CrossChasm",
+        level:"Silver_Competition",
+        website: "http://www.crosschasm.com",
+        logo: "img/logo-crosschasm.png"
+    },
+    {
+        name: "Siemens",
+        level:"Silver_Competition",
+        website: "http://www.siemens.com/plm/",
+        logo: "img/siemens.jpg"
+    },
+    {
+        name: "CD-Adapco",
+        level:"Bronze_Competition",
+        website: "http://www.cd-adapco.com/",
+        logo: "img/cdadapco.png"
+    },
+    {
+        name: "Vector",
+        level:"Bronze_Competition",
+        website: "http://www.vector.com/",
+        logo: "img/vector.png"
+    },
+    {
+        name: "GKN Driveline",
+        level:"Bronze_Competition",
+        website: "http://www.gkn.com/driveline/Pages/default.aspx",
+        logo: "img/gkn.jpg"
+    },
+    {
+        name: "Sensors",
+        level:"Bronze_Competition",
+        website: "http://www.sensors-inc.com/",
+        logo: "img/Sensorsogo.png"
+    },
+    {
+        name: "New Eagle",
+        level:"Bronze_Competition",
+        website: "http://www.neweagle.net/",
+        logo: "img/New Eagle_2013_website.jpg"
+    },
+    {
+        name: "Blackberry",
+        level:"Bronze_Competition",
+        website: "http://www.blackberry.com/academic",
+        logo: "img/blackberry.png"
+    },
+    {
+        name: "QNX Software Systems",
+        level:"Bronze_Competition",
+        website: "http://www.qnx.com",
+        logo: "img/qnx.png"
+    },
+    {
+        name: "Woodward",
+        level:"Bronze_Competition",
+        website: "http://www.woodward.com/",
+        logo: "img/woodward.png"
+    },
+    {
+        name: "Gage",
+        level:"Bronze_Competition",
+        website: "http://www.gageproducts.com",
+        logo: "img/Gage_website.jpg"
+    },
+    {
+        name: "The Delphi Foundation",
+        level:"Bronze_Competition",
+        website: "http://www.delphi.com",
+        logo: "img/delphi.png"
+    },
+    {
+        name: "Caterpillar",
+        level:"Bronze_Competition",
+        website: "http://www.cat.com/home",
+        logo: "img/caterpillar.png"
+    },
+    {
+        name: "Ricardo",
+        level:"Bronze_Competition",
+        website: "http://www.ricardo.com",
+        logo: "img/ricardo.jpg"
+    },
+    {
+        name: "Proterra",
+        level:"Bronze_Competition",
+        website: "http://www.proterra.com",
+        logo: "img/proterra.jpg"
+    },
+    {
+        name: "United States Environmental Protection Agency",
+        level:"Bronze_Competition",
+        website: "http://www.epa.gov/",
+        logo: "img/epa_logo-275x300.jpg"
+    }
+];
