09926c2d4d4d82dafbd1083f074b4082686cb326
Koding <ivy.cm.wong@gmail.com>
2015-09-22 17:38:57 +0000

    Changed .content padding-top to be based on rem. Added css to .content to resize based on vh and vw. Comments added in app.js to resizefontsize on why it might be causing bugs

--- js/app.js
+++ js/app.js
@@ -4,11 +4,11 @@ $(document).ready(function() {
     $('footer').css('top', $(document).height());
     $('#logo').css("height", $('nav').height());
     $('#name').css("height", $('nav').height() / 2);
-    $('.content').css('padding-top', $('.navbar').height() + 10 + 'px'); //Adds padding to ng-view to push it beneath the nav bar
+    $('.content').css('padding-top', '5.5rem'); //Adds padding to ng-view to push it beneath the nav bar
     $(document).on('resize', function() {
         resizeFontSize();
     });
-    populateSponsorsList();
+    //populateSponsorsList();
 });
 
 $(window).resize(function() {
@@ -22,19 +22,19 @@ function resizeFontSize() {
     window.window_width = $(window).width();
     window.window_height = $(window).height();
     var aspectRatio =  window.window_width / window.window_height;
-    body.css('margin-left', 0).css('margin-top', 0);
+    //body.css('margin-left', 0).css('margin-top', 0);
     if (aspectRatio > (16 / 9)) {
         window.rem = window.window_height / 67.5;
-        offSet = (window.window_width - body.width()) / 2;
-        body.css('margin-left', offSet);
+    //    offSet = (window.window_width - body.width()) / 2;
+    //    body.css('margin-left', offSet); // causing the shift left when scaling up
     } else {
         window.rem = window.window_width / 120;
-        offSet = (window.window_height - body.height()) / 2;
-        body.css('margin-top', offSet);
+    //    offSet = (window.window_height - body.height()) / 2;
+    //    body.css('margin-top', offSet);
     }
-    $('#logo').css("height", nav.height());
-    $('#name').css("height", nav.height() / 2);
-    $('html').css('font-size', window.rem);
+    //$('#logo').css("height", nav.height()); //looks unesscary; bootstrap nav does not appear to change size upon resize
+    //$('#name').css("height", nav.height() / 2); //looks unesscary; bootstrap nav does not appear to change size upon resize
+    $('.content').css('font-size', window.rem); //changed to .content to prevent changing the font-size of the nav bar
 }
 
 function populateSponsorsList() {
